1cb3f962d0e31c3b33910c332790c97b4d03317c
lromano97 <lromano@est.frba.utn.edu.ar>
2017-10-27 00:00:02 -0300

    Subo aproximacion exponencial

--- ajusteDeErrores.m
+++ ajusteDeErrores.m
@@ -7,17 +7,26 @@ endfunction
 
 function aproximacionPotencial(listX, listY)
   cantFilas = size(listX)
-  matrizAproximacion = zeros(cantFilas(1,2), 4)
+  matrizAproximacion = zeros(cantFilas(1,2)+1, 6)
   for i = 1:cantFilas(1,2)
-    matrizAproximacion(i, 1) = reallog(listX(1,i))
-    matrizAproximacion(i, 2) = reallog(listX(1,i)).**2
-    matrizAproximacion(i,3) = reallog(listY(1,i))
-    matrizAproximacion(i,4) = reallog(listX(1,i))*reallog(listY(i,1))
+    matrizAproximacion(i,1) = listX(1,i)
+    matrizAproximacion(i,2) = listY(1,i)
+    matrizAproximacion(i, 3) = reallog(listX(1,i))
+    matrizAproximacion(i, 4) = reallog(listX(1,i)).**2
+    matrizAproximacion(i,5) = reallog(listY(1,i))
+    matrizAproximacion(i,6) = reallog(listX(1,i))*reallog(listY(1,i))
   endfor
-  sumatoriaLNX = sum(matrizAproximacion(:,1))
-  sumatoriaLNXCuadrado = sum(matrizAproximacion(:,2));
-  sumatoriaLNY = sum(matrizAproximacion(:,3));
-  sumatoriaLNXLNY = sum(matrizAproximacion(:,4));
+  matrizAproximacion(cantFilas(1,2)+1,1) = sum(matrizAproximacion(:,1))
+  matrizAproximacion(cantFilas(1,2)+1,2) = sum(matrizAproximacion(:,2))
+  matrizAproximacion(cantFilas(1,2)+1,3) = sum(matrizAproximacion(:,3))
+  matrizAproximacion(cantFilas(1,2)+1,4) = sum(matrizAproximacion(:,4))
+  matrizAproximacion(cantFilas(1,2)+1,5) = sum(matrizAproximacion(:,5))
+  matrizAproximacion(cantFilas(1,2)+1,6) = sum(matrizAproximacion(:,6))  
+  Matrix1 = [matrizAproximacion(cantFilas(1,2)+1,4), matrizAproximacion(cantFilas(1,2)+1,3) ; matrizAproximacion(cantFilas(1,2)+1,3), cantFilas(1,2)]
+  Matrix2 = [matrizAproximacion(cantFilas(1,2)+1,6); matrizAproximacion(cantFilas(1,2)+1,5)]
+  Solucion = Matrix1\Matrix2
+  sizeMatrix2 = size(Matrix2)
+  Solucion(sizeMatrix2(1,1),1) = exp(Solucion(sizeMatrix2(1,1),1))
 endfunction
 
 
@@ -30,12 +39,12 @@ endfunction
 
 function aproximacionLineal(listX, listY)
   cantFilas = size(listX);
-  matrizAproximacion = zeros(cantFilas(1,1), 2);
+  matrizAproximacion = zeros(cantFilas(1,2)+1, 2);
   for i = 1:cantFilas
     matrizAproximacion(i,1) = listX(i,1).**2;
     matrizAproximacion(i,2) = listX(i,1)*listY(i,1);
   endfor
-  sumatoriaXCuadrado = sum(matrizAproximacion(:,1));
+  matrizAproximacion(cantFilas(1,2)+1,1) = sum(matrizAproximacion(:,1));
   sumatoriaXY = sum(matrizAproximacion(:,2));
   sumatoriaX = sum(listX(:,1));
   sumatoriaY = sum(listY(:,1));
